3ad131733b59d11bb63a1b5a3e44bb3c29404559
andredev <andluccag@gmail.com>
2025-09-15 11:56:11 -0300

    commit final list page backend

--- RemeberMeBackEnd/Middlewares/ReviewResps.js
+++ RemeberMeBackEnd/Middlewares/ReviewResps.js
@@ -19,6 +19,20 @@ export function isLong(str){
     return false;
 }
 
+export function isLongTitle(str){
+    if(str.length > 30){
+        return true;    
+    }
+    return false;
+}
+
+export function isLongDesc(str){
+    if(str.length > 300){
+        return true;    
+    }
+    return false;
+}
+
 export function isDiferent(strone, strtwo){
     if(strone !== strtwo){
         return true
