debba4ba60a27fb6b07017c2b6e544b09d85bba2
André Lucca Gaem Lorena Robim <tomatv30@gmail.com>
2025-09-15 21:37:45 -0300

    Update BD.js

--- RemeberMeBackEnd/Models/BD.js
+++ RemeberMeBackEnd/Models/BD.js
@@ -1,9 +1,14 @@
 import {Sequelize} from "sequelize";
 
-const sequelize = new Sequelize('remebermebd', 'root', '', {
-    host: "localhost",
-    dialect: "mysql",
-    logging: false
-})
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Importante para Aiven
+    }
+  },
+  logging: console.log // Para ver queries no log
+});
 
 export default {sequelize, Sequelize}
