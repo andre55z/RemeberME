a45e7ec5bcc36061bee3f9938145aacc3dcff51b
andredev <andluccag@gmail.com>
2025-09-17 22:04:24 -0300

    Commit de fix dos realoads

--- RemeberMeFrontEnd/src/components/ListPage/Edit.jsx
+++ RemeberMeFrontEnd/src/components/ListPage/Edit.jsx
@@ -38,7 +38,7 @@ function Edit({setFunction, titulo, descricao, id}){
             {erro && <ErrorNotif message={erroMsg} ClassName={" h-[100px] mt-[20px] w-[300px] text-center p-7 rounded-[20px] font-inter"} colorNotif={"bg-red-500"}/>}
             <div className="lg:w-[50%] w-[80%] h-[60%] lg:h-[70%] bg-gradient-to-br from-pink-500 to-purple-700 mt-[5%] rounded-[30px] flex flex-col items-center">
                 <Input type="text" 
-                    ClassName={"w-[150%]  ml-[-25%] mt-[10%] lg:h-15 h-10 font-inter text-[70%] lg:text-[100%] transition-transform hover:scale-105 ease-in-out"}
+                    ClassName={"w-[143%]  ml-[-20%] mt-[10%] lg:h-15 h-10 font-inter text-[70%] lg:text-[100%] transition-transform hover:scale-105 ease-in-out"}
                     variable={newTitle}
                     setFunction={setNewTitle}/>
                 <textarea 
