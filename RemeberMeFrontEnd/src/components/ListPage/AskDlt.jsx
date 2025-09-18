26369a0804c7e7eb70118987949627a9e736a496
andredev <andluccag@gmail.com>
2025-09-17 21:30:55 -0300

    Commit de fix dos realoads

--- RemeberMeFrontEnd/src/components/ListPage/AskDlt.jsx
+++ RemeberMeFrontEnd/src/components/ListPage/AskDlt.jsx
@@ -22,7 +22,7 @@ function AskDlt({setAskDltComp, idForDelete}){
         <div className="bg-black/70 w-full h-full absolute  z-50 flex flex-col items-center">
             <div className="bg-purple-700 w-[80%] lg:w-[30%] h-[25%] lg:h-[35%] rounded-[30px] lg:mt-[10%] mt-[40%] flex flex-col items-center text-center">
                 <h3 className="text-[150%] mt-[10%] font-concertone">Tem certeza que deseja excluir a task?</h3>
-                <div className="flex flex-row items-center gap-[20%] ml-[-18%] lg:ml-0 mt-[20%] ">
+                <div className="flex flex-row items-center p-1 gap-[20%] ml-[-18%] lg:ml-0 mt-[20%] ">
                     <Button palavraNoBotao={"Cancelar"} ClassName={"w-[100%] lg:w-[200px] lg:h- rounded-[10%] p-[10%] h-[100%] bg-button hover:bg-pink-300 transition-all duration-200"} onClickFunction={closeDlt}/>
                     <Button palavraNoBotao={"Excluir"} ClassName={"bg-red-600 p-[10%] rounded-[10%] w-[100%] h-[100%] hover:bg-red-400 transition-all duration-200"} onClickFunction={()=>deleting(idForDelete)}/>
                 </div>
--- RemeberMeFrontEnd/src/components/ListPage/TaskBox.jsx
+++ RemeberMeFrontEnd/src/components/ListPage/TaskBox.jsx
@@ -4,13 +4,13 @@ import Button from "../Button";
 function TaskBox({onClickFunction, setTitulo, setDescricao, titulo, descricao, onClickSuccess}){
 
     return(
-        <div className="lg:w-[50%] w-[80%] h-[70%] bg-gradient-to-br from-pink-500 to-purple-700 mt-[5%] rounded-[30px] flex flex-col items-center">
+        <div className="lg:w-[70%] w-[80%] h-[70%] bg-gradient-to-br from-pink-500 to-purple-700 mt-[5%] rounded-[30px] flex flex-col items-center">
             <Input type="text" placeHolder={"Insira o titulo"} 
             ClassName={"h-20 mt-[20%] lg:w-[170%] w-[80%] outline-none lg:ml-[-34%] ml-[10%] font-inter text-[120%] transition-transform hover:scale-105 ease-in-out"}
             setFunction={setTitulo}
             variable={titulo}/> 
             <textarea 
-                className="w-[80%] mt-[5%] outline-none transition-transform hover:scale-105 ease-in-out h-100 rounded-[20px] ml-[1%] p-4 resize-y font-inter text-[110%] bg-amber-50" 
+                className="w-[80%] lg:w-[90%] mt-[5%] outline-none transition-transform hover:scale-105 ease-in-out h-100 rounded-[20px] ml-[1%] p-4 resize-y font-inter text-[110%] bg-amber-50" 
                 placeholder="Descreva sua tarefa..."
                 value={descricao}
                 onChange={(e)=>{setDescricao(e.target.value)}}
