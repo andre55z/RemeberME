684fb2cfb866287f30099bad4cbf1323c9b18ab0
andredev <andluccag@gmail.com>
2025-09-17 22:55:30 -0300

    Commit das fixs de api no bd

--- RemeberMeBackEnd/Models/BD.js
+++ RemeberMeBackEnd/Models/BD.js
@@ -5,10 +5,20 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false // Importante para Aiven
+      rejectUnauthorized: false 
     }
   },
-  logging: console.log // Para ver queries no log
+  retry: {
+    max: 3,
+    timeout: 5000
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  logging: console.log 
 });
 
 export default {sequelize, Sequelize}
