10eb39f9d81b411965a212b31e77af2039e04df4
andredev <andluccag@gmail.com>
2025-09-18 18:32:13 -0300

    Commit do fix do botão de adc tasks

--- RemeberMeFrontEnd/src/pages/ListPage.jsx
+++ RemeberMeFrontEnd/src/pages/ListPage.jsx
@@ -104,7 +104,7 @@ function ListPage(){
 
             <div className="top-[30px] bottom-[-45%] lg:mt-0 mt-[10%] ml-[10%]">
                 <Button 
-                    ClassName=' lg:h-[100px] lg:w-[100px] h-[10%] w-[25%] mb-10 lg:pb-1 pb-4 bg-button  rounded-[100%] mt-0 text-[50px]' 
+                    ClassName=' lg:h-[100px] lg:w-[100px] h-[50px] w-[50px] mb-10 lg:pb-1 pb-4 bg-button  rounded-[100%] mt-0 text-[50px]' 
                     palavraNoBotao="+" 
                     onClickFunction={()=>setNewTask(true)}
                 />
