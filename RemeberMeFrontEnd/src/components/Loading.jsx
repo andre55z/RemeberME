5d834661302dbd28d498853508c7141a6308ca26
aluno.local <tomatv30@gmail.com>
2025-09-19 12:21:47 -0300

    Commit de inicio do widget de loading

---
+++ RemeberMeFrontEnd/src/components/Loading.jsx
@@ -0,0 +1,9 @@
+function Loading(){
+    return(
+        <div className="bg-black/70 w-full h-full absolute mix-blend-color:multiply  z-4 flex flex-col items-center">
+            <p className="text-[50px] font-color-secundary mt-[30%]">Um momento...</p>
+        </div>
+    )
+}
+
+export default Loading;
